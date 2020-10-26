1b53439928ee7e2a4f764cbdb8e35f4aaa3b0282
Davide de Lerma <davide.delerma@sa.catapult.org.uk>
2020-10-26 15:58:01 +0000

    fix create ncep folder before downloading

--- restore_ncep_data.sh
+++ restore_ncep_data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/ncep/ /usr/local/mcep/
+mkdir /usr/local/ncep
+aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/ncep/ /usr/local/ncep/
